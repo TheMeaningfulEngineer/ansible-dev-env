f3823d9f65063cf437646a50edeca99b3ed34344
Alan <alan.martinovic@gmail.com>
2022-07-26 19:30:46 +0200

    Add possibility to ignore files with .rgignore in the project dir
    A new function was created for find_files.
    Was lucky enough to have find_files accept the find command.
    
    A different approach was needed for grep where a setting on the level of
    telescope needed to be set (`vimgrep_arguments`).
    
    As a result of this there is some inconsistency where some keybindings
    are done with pure lua and others are viml.
    
    Also, passing the theme to `find_files` while it has parameters means
    first passing the parameters to the theme and then just passing the
    theme to the picker.

--- roles/nvim_appimage/files/nvim/lua/user/telescope.lua
+++ roles/nvim_appimage/files/nvim/lua/user/telescope.lua
@@ -1,24 +1,15 @@
-vim.keymap.set('n', '<Leader>f', ':Telescope find_files theme=ivy <CR>', {silent = true})
-vim.keymap.set('n', '<Leader>r', ':Telescope live_grep theme=ivy <CR>', {silent = true})
-vim.keymap.set('n', '<C-p>', ':Telescope buffers theme=ivy <CR>')
-vim.keymap.set('n', '<C-n>', ':Telescope buffers theme=ivy <CR>')
-
--- local action_state = require "telescope.actions.state"
--- function delete_buffer_custom(prompt_bufnr)
---   local current_picker = action_state.get_current_picker(prompt_bufnr)
--- 
---   function delete_selection_function(selection)
---     if vim.api.nvim_buf_get_option(selection.bufnr, "buftype") == "terminal" then
---       vim.api.nvim_buf_delete(selection.bufnr, { force = true })
---     else
---       vim.api.nvim_buf_delete(selection.bufnr, { force = false })
---     end
---   end
---   current_picker:delete_selection(delete_selection_function)
--- end
-
-
 require("telescope").setup {
+  defaults = {
+    path_display = {"truncate"},
+    vimgrep_arguments = {"rg", 
+                         "--color=never",
+                         "--no-heading",
+                         "--with-filename",
+                         "--line-number",
+                         "--column",
+                         "--smart-case",
+                         "--no-ignore-vcs"}
+  },
   pickers = {
     buffers = {
       show_all_buffers = true,
@@ -26,8 +17,20 @@ require("telescope").setup {
       mappings = {
         i = {
           ["<c-d>"] = "delete_buffer",
-        }
+j       }
       }
     }
   }
 }
+
+function find_files()
+    function_name = {"rg", "--files", "--no-ignore-vcs" }
+    themes = require('telescope.themes')
+    builtin = require('telescope.builtin')
+    builtin.find_files(themes.get_ivy({find_command = function_name}))
+end
+
+vim.keymap.set('n', '<Leader>f', find_files)
+vim.keymap.set('n', '<Leader>r', ':Telescope live_grep theme=ivy<CR>')
+vim.keymap.set('n', '<C-p>', ':Telescope buffers theme=ivy <CR>')
+vim.keymap.set('n', '<C-n>', ':Telescope buffers theme=ivy <CR>')
