d116c1dc287faaf967e2b0e3023e205e671fe15e
Alan Martinovic <alan.martinovic@gmail.com>
2024-05-26 22:12:55 +0200

    Migrate to the lazy loading structure

--- roles/nvim_appimage/files/nvim/lua/plugins/init.lua
+++ roles/nvim_appimage/files/nvim/lua/plugins/init.lua
@@ -1,35 +1,12 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
-
-require("lazy").setup({
+return {
   "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
 
   -- Treesitter
   -- Some workaround for installing with packer
   -- (https://github.com/nvim-treesitter/nvim-treesitter/issues/3135)
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = function() require("nvim-treesitter.install").update { with_sync = true } end
-  },
   { "navarasu/onedark.nvim", as = "onedark" },
   "nvim-treesitter/playground",
-
-  -- Telescope
-  "nvim-telescope/telescope.nvim",
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 
   "dhruvasagar/vim-zoom",
   "qpkorr/vim-bufkill",
@@ -46,7 +23,4 @@ require("lazy").setup({
   -- snippets
   "L3MON4D3/LuaSnip", -- snippet engine
   "rafamadriz/friendly-snippets", -- a bunch of snippets to use
-})
-
-
-
+}
---
+++ roles/nvim_appimage/files/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "catppuccin/nvim", name = "catppuccin" }
+  },
+  build = function() 
+    require("nvim-treesitter.install").update { with_sync = true } 
+  end,
+  config = function()
+    -- Setup nvim-treesitter
+    require('nvim-treesitter.configs').setup {
+      -- one of "all", "maintained" (parsers with maintainers),
+      -- or a list of languages
+      ensure_installed = { "lua", "python", "markdown", "bash", "comment" },
+      playground = {
+        enable = true,
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      }
+    }
+
+    -- Setup catppuccin colorscheme
+    require("catppuccin").setup({
+      integrations = {
+        treesitter = true,
+        markdown = true,
+      },
+    })
+
+    -- Apply catppuccin colorscheme only for Markdown files
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function()
+        vim.cmd("colorscheme catppuccin")
+      end,
+    })
+  end
+}
