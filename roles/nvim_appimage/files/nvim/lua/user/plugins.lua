b34a79799a36c5f468c4ee28b540acb370e56a75
Alan Martinovic <alan.martinovic@gmail.com>
2024-05-26 21:06:19 +0200

    Migration to lazy iteration 1

--- roles/nvim_appimage/files/nvim/lua/user/plugins.lua
+++ roles/nvim_appimage/files/nvim/lua/user/plugins.lua
@@ -1,85 +1,52 @@
---Package management
-
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
-
--- Install your plugins here
-return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-
+require("lazy").setup({
+  "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
 
   -- Treesitter
   -- Some workaround for installing with packer
   -- (https://github.com/nvim-treesitter/nvim-treesitter/issues/3135)
-  use {"nvim-treesitter/nvim-treesitter",
-        run = function() require("nvim-treesitter.install").update { with_sync = true } end}
-  use { "navarasu/onedark.nvim", as = "onedark" }
-  use "nvim-treesitter/playground"
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function() require("nvim-treesitter.install").update { with_sync = true } end
+  },
+  { "navarasu/onedark.nvim", as = "onedark" },
+  "nvim-treesitter/playground",
 
   -- Telescope
-  use "nvim-telescope/telescope.nvim"
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  "nvim-telescope/telescope.nvim",
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 
+  "dhruvasagar/vim-zoom",
+  "qpkorr/vim-bufkill",
+  "karoliskoncevicius/vim-sendtowindow",
 
-  use "dhruvasagar/vim-zoom"
-  use "qpkorr/vim-bufkill"
-  use "karoliskoncevicius/vim-sendtowindow"
-
-    -- cmp pluginas
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
+  -- cmp plugins
+  "hrsh7th/nvim-cmp", -- The completion plugin
+  "hrsh7th/cmp-buffer", -- buffer completions
+  "hrsh7th/cmp-path", -- path completions
+  "hrsh7th/cmp-cmdline", -- cmdline completions
+  "saadparwaiz1/cmp_luasnip", -- snippet completions
+  "hrsh7th/cmp-nvim-lsp",
 
   -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  "L3MON4D3/LuaSnip", -- snippet engine
+  "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+})
 
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+
