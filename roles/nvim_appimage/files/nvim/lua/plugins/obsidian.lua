cdc77f1d60ae4d6c8c5fc9dc5982f5be3b309124
Alan <alan.martinovic@gmail.com>
2024-09-01 18:24:08 +0200

    Have obsidian.nvim create new notes in the same manner as Obsidian the app

--- roles/nvim_appimage/files/nvim/lua/plugins/obsidian.lua
+++ roles/nvim_appimage/files/nvim/lua/plugins/obsidian.lua
@@ -5,7 +5,6 @@ return {
     -- Required.
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp", -- The completion plugin
-
   },
   opts = {
     workspaces = {
@@ -15,25 +14,19 @@ return {
       },
     },
     ui = {
-        enable = false,
+      enable = false,
     },
     wiki_link_func = "use_alias_only",
     note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-      local suffix = ""
-      if title ~= nil then
-        -- If title is given, transform it into valid file name.
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
-        end
-      end
-      return tostring(os.time()) .. "-" .. suffix
+      -- Convert the title to a valid file name.
+      -- This removes any special characters and replaces spaces with dashes.
+      local id = title:gsub("%s+", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      return id
     end,
-
+    note_path_func = function(spec)
+      -- Construct the file path using the sanitized title.
+      local path = spec.dir / spec.id
+      return path:with_suffix(".md")
+    end,
   },
 }
