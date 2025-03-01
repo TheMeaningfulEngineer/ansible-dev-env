bf839c12f8d59da8272255a062405b21f8e3f8e3
Alan <alan.martinovic@northern.tech>
2025-03-01 22:31:46 +0100

    revamp treesitter and block selection

--- roles/nvim_appimage/files/nvim/lua/plugins/treesitter.lua
+++ roles/nvim_appimage/files/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,41 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    { "catppuccin/nvim", name = "catppuccin" }
+    "nvim-treesitter/playground", -- Treesitter debugging
+    "nvim-treesitter/nvim-treesitter-textobjects", -- Text objects support
+    { "catppuccin/nvim", name = "catppuccin" } -- Theme integration
   },
-  build = function() 
-    require("nvim-treesitter.install").update { with_sync = true } 
+  build = function()
+    require("nvim-treesitter.install").update { with_sync = true }
   end,
   config = function()
-    -- Setup nvim-treesitter
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { "lua", "python", "markdown", "markdown_inline", "bash", "comment", "dockerfile", "json", "bitbake"},
+      modules = {}, -- Explicitly defined to satisfy Lua LSP
+      ensure_installed = {
+        "lua", "python", "markdown", "markdown_inline",
+        "bash", "comment", "dockerfile", "json", "bitbake"
+      },
+      sync_install = false,
+      auto_install = true,
+      ignore_install = {},
+
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
-      }
+      },
+      playground = { enable = true },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Jump to next object
+          keymaps = {
+            ["ac"] = "@block.outer", -- Select around block (includes backticks)
+            ["ic"] = "@block.inner", -- Select inside block (excludes backticks)
+          },
+        }
+      },
     }
   end
 }
+
