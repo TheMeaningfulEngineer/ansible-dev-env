ff26d11f0f0f8e8d7a6e5d99ee230b653f31fb67
Alan <alan.martinovic@gmail.com>
2025-03-01 22:31:46 +0100

    revamp treesitter and block selection

--- roles/nvim_appimage/files/nvim/lua/plugins/init.lua
+++ roles/nvim_appimage/files/nvim/lua/plugins/init.lua
@@ -1,21 +1,6 @@
 return {
-  "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
-
-  -- Treesitter
-  -- Some workaround for installing with packer
-  -- (https://github.com/nvim-treesitter/nvim-treesitter/issues/3135)
-  "nvim-treesitter/playground",
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      integrations = {
-        treesitter = true,
-        markdown = true,
-      },
-    },
-  },
+  "nvim-lua/plenary.nvim", -- Useful Lua functions
   "dhruvasagar/vim-zoom",
   "qpkorr/vim-bufkill",
-  "karoliskoncevicius/vim-sendtowindow",
+  "karoliskoncevicius/vim-sendtowindow"
 }
